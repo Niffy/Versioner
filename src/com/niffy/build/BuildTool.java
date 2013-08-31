2797d3c1806048ef5c07b916df941e9371a02a1d
Paul Robinson <niffydroid@gmail.com>
2013-08-31 17:18:55 +0100

    Can now build!

--- src/com/niffy/build/BuildTool.java
+++ src/com/niffy/build/BuildTool.java
@@ -1,5 +1,11 @@
 package com.niffy.build;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.Properties;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -7,7 +13,7 @@ public class BuildTool {
 	// ===========================================================
 	// Constants
 	// ===========================================================
-	@SuppressWarnings("unused")
+	@SuppressWarnings("unused") 
 	private final static Logger log = LoggerFactory.getLogger(BuildTool.class);
 
 	// ===========================================================
@@ -18,10 +24,32 @@ public class BuildTool {
 	// Constructors
 	// ===========================================================
 
-	public BuildTool() {
 
+	public static void main(String[] args) {
+		System.out.println("Started");
+		
+		ClassLoader cl = ClassLoader.getSystemClassLoader();
+		 
+        URL[] urls = ((URLClassLoader)cl).getURLs();
+ 
+        for(URL url: urls){
+        	System.out.println(url.getFile());
+        }
+        
+		
+		
+		Properties prop = new Properties();
+		try{
+			InputStream stream = BuildTool.class.getClassLoader().getResourceAsStream("log4j.properties");
+			prop.load(stream);
+			System.out.println("Done it!!");
+		} catch (IOException ex){
+			ex.printStackTrace();
+		}
+		
+		
+		
 	}
-
 	// ===========================================================
 	// Methods for/from SuperClass/Interfaces
 	// ===========================================================
